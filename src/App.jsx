6cfd6a5b95c202c78157b547be276f1102b8b5b7
Rashedalidesigner <rashedalidesigner@gmail.com>
2025-09-01 15:35:34 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from './components/Hero'
+import Header from './components/Header'
+
+function App() {
+  return (
+    <div className='app'>
+      <Header/>
+      <Hero/>
+    </div>
+  )
+}
+
+export default App
